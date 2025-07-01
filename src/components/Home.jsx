200903f8e41a99dc8b9b689e1710c785716c85bf
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-01 13:01:07 +0530

    Home part 1 completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="w-full h-[89vh] bg-[#3E4E2C]">
+      <div className="w-1/2 h-full flex items-center flex-col pt-30 pl-13 gap-10">
+        <h1 className="text-8xl text-white font-[LoveDays]">
+          Helping Gardens Speak, One Story at a Time. 🌿✨
+        </h1>
+        <button className="px-6 py-4 bg-[#F9A826] rounded-2xl -ml-140 flex items-center">
+          Explore <FaArrowRightLong className="ml-5 mt-1" />
+        </button>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
