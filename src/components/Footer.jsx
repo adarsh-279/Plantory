2f822c2c27bcc96da4731972d35d6631955442b0
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-02 18:50:57 +0530

    Footer Part 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,19 @@ import { ImLeaf } from "react-icons/im";
 
 const Footer = () => {
   return (
-    <div className="w-full h-40 px-4 py-3 bg-[#3E4E2C]">
+    <div className="w-full h-40 px-4 py-3 bg-[#3E4E2C] text-white ">
       <div className="w-full h-full flex flex-row items-center justify-between">
-        <div className="w-1/3 h-full px-5 gap-2 flex flex-row items-center text-white font-[Tothepoint]">
+        <div className="w-1/3 h-full px-5 gap-2 flex flex-row items-center font-[Tothepoint]">
           <ImLeaf className="w-22 h-11" />
           <h1 className="text-6xl pt-5">Plantory</h1>
+        </div>
+        <div className="w-1/3 h-full flex flex-col text-center justify-center">
+          <h1 className="mb-3">Socials</h1>
+          <div className="flex flex-col  opacity-70">
+            <a href="#">Instagram</a>
+            <a href="#">Youtube</a>
+            <a href="#">Facebook</a>
+          </div>
         </div>
         <div className="w-1/3 h-full"></div>
         <div className="w-1/3 h-full"></div>
