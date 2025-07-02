595082c6c41a873f3020b4f8c3166358fbfa0faf
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-02 19:01:53 +0530

    Updated PreFooter

--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -3,7 +3,7 @@ import { FaHeart } from "react-icons/fa";
 
 const PreFooter = () => {
   return (
-    <div className="w-full h-full flex flex-row px-5 bg-[#FAF9F6] pt-40">
+    <div className="w-full h-full flex flex-row px-5 bg-[#FAF9F6] pt-40 pb-2">
       <h1 className="flex items-center gap-2 text-6xl italic opacity-8">
         Made with love <FaHeart />
       </h1>
