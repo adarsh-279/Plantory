28df44965de5727cfea4b1f265757eba16afc529
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-05 17:28:33 +0530

    Routing Completed

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -4,15 +4,31 @@ import Home from '../components/Home'
 import Tips from '../components/Tips'
 import CardComponent from '../components/CardComponent'
 import Footer from '../components/Footer'
+import Indoor from '../components/plants/Indoor'
+import Outdoor from '../components/plants/Outdoor'
+import Medicinal from '../components/plants/Medicinal'
+import Flowering from '../components/plants/Flowering'
+import Decorative from '../components/plants/Decorative'
+import LowMaintenance from '../components/plants/LowMaintenance'
+import PetFriendly from '../components/plants/PetFriendly'
+import RareExotic from '../components/plants/RareExotic'
 
 const Routing = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category" element={<CardComponent/>} />
+        <Route path="" element={<Home />} />
+        <Route path="/category" element={<CardComponent />} />
         <Route path="/tips" element={<Tips />} />
         <Route path="/contact" element={<Footer />} />
+        <Route path="/Indoor" element={<Indoor />} />
+        <Route path="/Outdoor" element={<Outdoor />} />
+        <Route path="/Medicinal" element={<Medicinal />} />
+        <Route path="/Flowering" element={<Flowering />} />
+        <Route path="/Decorative" element={<Decorative />} />
+        <Route path="/LowMaintenance" element={<LowMaintenance />} />
+        <Route path="/PetFriendly" element={<PetFriendly />} />
+        <Route path="/RareExotic" element={<RareExotic />} />
       </Routes>
     </>
   );
