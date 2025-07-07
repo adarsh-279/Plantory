96a110ab0fd20a295574405a144bd82d8566e9f8
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-07 13:55:18 +0530

    Navbar Responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,30 +11,30 @@ const Navbar = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="w-full h-20 px-10 py-3 bg-[#3E4E2C] text-white border-b-4 border-[#273A12] flex items-center justify-between font-[Tothepoint]"
+      className="w-full h-20 px-3 lg:px-10 py-3 bg-[#3E4E2C] text-white border-b-4 border-[#273A12] flex items-center justify-between font-[Tothepoint]"
     >
-      <div className=" flex items-center gap-5">
-        <ImLeaf className="w-12 h-6" />
+      <div className=" flex items-center gap-1 lg:gap-5">
+        <ImLeaf className="w-8 h-5 lg:w-12 lg:h-6" />
         <h1 className="text-3xl pt-2">Plantory</h1>
       </div>
 
       <div className="w-1/2 text-3xl pt-2 flex items-center justify-evenly">
-        <ul className="flex flex-row items-center justify-center gap-15">
+        <ul className="flex flex-row items-center justify-center gap-3 lg:gap-15">
           <NavLink to="">
-            <motion.li whileHover={{scale:1.12}}>Home</motion.li>
+            <motion.li whileHover={{ scale: 1.12 }}>Home</motion.li>
           </NavLink>
           <NavLink to="/category">
-            <motion.li whileHover={{scale:1.12}}>Category</motion.li>
+            <motion.li whileHover={{ scale: 1.12 }}>Category</motion.li>
           </NavLink>
           <NavLink to="/tips">
-            <motion.li whileHover={{scale:1.12}}>Tips</motion.li>
+            <motion.li whileHover={{ scale: 1.12 }}>Tips</motion.li>
           </NavLink>
         </ul>
       </div>
 
-      <div className="flex items-center gap-5">
-        <FaHeart className="w-12 h-6" />
-        <FaUserAlt className="w-12 h-6" />
+      <div className="flex items-center lg:gap-5">
+        <FaHeart className="w-8 h-5 mt-2 lg:w-12 lg:h-6" />
+        <FaUserAlt className="w-8 h-5 mt-2 lg:w-12 lg:h-6" />
       </div>
     </motion.div>
   );
