50de59d1827ad0ea68b082de80ffb8acddb14ccf
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-07 17:34:04 +0530

    PreFooter & Footer Responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="w-full h-40 px-4 py-3 bg-[#3E4E2C] text-white ">
+    <div className="w-full h-30 lg:h-40 px-4 py-3 bg-[#3E4E2C] text-white ">
       <div className="w-full h-full flex flex-row items-center justify-between">
-        <div className="w-1/3 h-full px-5 gap-2 flex flex-row items-center font-[Tothepoint]">
+        <div className="w-1/3 h-full px-5 gap-3 lg:gap-2 flex flex-row items-center font-[Tothepoint]">
           <ImLeaf className="w-22 h-11" />
-          <h1 className="text-6xl pt-5">Plantory</h1>
+          <h1 className="text-3xl lg:text-6xl -pt-2 lg:pt-5">Plantory</h1>
         </div>
-        <div className="w-1/3 h-full flex flex-col text-center justify-center">
+        <div className="w-1/3 h-full flex flex-col text-center justify-center text-xs lg:text-lg">
           <h1 className="mb-3">Socials</h1>
-          <div className="flex flex-col  opacity-70">
+          <div className="flex flex-col opacity-70 gap-1 lg:gap-0">
             <a href="#">Instagram</a>
             <a href="#">Youtube</a>
             <a href="#">Facebook</a>
@@ -20,7 +20,7 @@ const Footer = () => {
         </div>
         <div
           id="contact"
-          className="w-1/3 h-full flex flex-col text-center justify-center"
+          className="w-1/3 h-full flex flex-col text-center justify-center text-xs lg:text-lg"
         >
           <h1 className="mb-3">Contacts</h1>
           <div className="flex flex-col  opacity-70">
@@ -28,8 +28,8 @@ const Footer = () => {
             <h1>contact@plantory.com</h1>
           </div>
         </div>
-        <div className="w-1/3 h-full pt-2 flex flex-col items-center justify-evenly opacity-70">
-          <ul className="flex flex-col items-center justify-center gap-5">
+        <div className="w-1/3 h-full pt-2 lg:flex flex-col items-center justify-evenly opacity-70 hidden">
+          <ul className="flex flex-col items-center justify-center gap-5 ">
             <NavLink to="">
               <li>Home</li>
             </NavLink>
--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -3,9 +3,9 @@ import { FaHeart } from "react-icons/fa";
 
 const PreFooter = () => {
   return (
-    <div className="w-full h-full flex flex-row px-5 bg-[#FAF9F6] pt-40 pb-2">
-      <h1 className="flex items-center gap-2 text-6xl italic opacity-8">
-        Made with love <FaHeart />
+    <div className="w-full h-full flex flex-row px-5 bg-[#FAF9F6] pt-15 lg:pt-25 pb-2">
+      <h1 className="flex items-center gap-2 text-4xl lg:text-6xl italic opacity-8">
+        Made with love <FaHeart className='w-8 h-7 lg:w-15 lg:h-10 lg:mt-4' />
       </h1>
     </div>
   );
